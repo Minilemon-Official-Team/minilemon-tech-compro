99b5bec25751a48c82c6004f5030a33ff430b2a2
JonathanLim46 <jonathanfrizy12@gmail.com>
2026-02-07 12:26:48 +0700

    add footer and product page

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -2,7 +2,7 @@ import ProductPage from '@/screens/product/Product';
 
 export default function Product() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+    <div className='flex min-h-screen bg-zinc-50 font-sans dark:bg-black'>
       <ProductPage />
     </div>
   );

0e7c99e95d0f5ba0ef8fbefa60d0b41bb9d5498d
haritsabdurr <abdurrahman.825190102@stu.untar.ac.id>
2026-02-10 10:12:26 +0700

    Adjust layout on some sections 10/02/2026 10:12

--- src/components/sections/collab/desktop/Hero.jsx
+++ src/components/sections/collab/desktop/Hero.jsx
@@ -1,43 +1,43 @@
-import { FounderCard } from "@/components/ui/Card";
+import { FounderCard } from '@/components/ui/Card';
 
 export default function Hero() {
   const founderDatas = [
     {
       id: 1,
-      name: "Lorem Ipsum",
-      subTitle: "Lorem Ipsum",
-      color: "#EABDF2",
+      name: 'Lorem Ipsum',
+      subTitle: 'Lorem Ipsum',
+      color: '#EABDF2',
     },
     {
       id: 2,
-      name: "Lorem Ipsum",
-      subTitle: "Lorem Ipsum",
-      color: "#03805E",
+      name: 'Lorem Ipsum',
+      subTitle: 'Lorem Ipsum',
+      color: '#03805E',
     },
     {
       id: 3,
-      name: "Lorem Ipsum",
-      subTitle: "Lorem Ipsum",
-      color: "#3ACA47",
+      name: 'Lorem Ipsum',
+      subTitle: 'Lorem Ipsum',
+      color: '#3ACA47',
     },
   ];
   return (
-    <div className="w-full min-h-screen px-6 grid grid-cols-1 lg:grid-cols-2 gap-24 items-center ">
-      <header className="relative z-20">
+    <div className='max-w-330 2xl:max-w-400 mx-auto min-h-screen grid grid-cols-1 lg:grid-cols-2 gap-24 items-center '>
+      <header className='relative z-20'>
         <p
-          aria-hidden="true"
-          className="absolute -top-28 left-0 text-8xl tracking-tight text-white/10 select-none"
+          aria-hidden='true'
+          className='absolute -top-28 left-0 text-8xl tracking-tight text-white/10 select-none'
         >
           COLLABS
         </p>
-        <h2 className="relative text-8xl font-bold tracking-tight text-white mb-8">
+        <h2 className='relative text-8xl font-bold tracking-tight text-white mb-8'>
           COLLABS
         </h2>
-        <p className="text-white text-xl mb-4">
+        <p className='text-white text-xl mb-4'>
           Building Technology Together Through Openness And Learning
         </p>
       </header>
-      <div className="flex flex-col gap-12">
+      <div className='flex flex-col gap-12'>
         {founderDatas.map((items) => (
           <FounderCard key={items.id} props={items} />
         ))}
--- src/screens/collab/Collab.desktop.jsx
+++ src/screens/collab/Collab.desktop.jsx
@@ -4,13 +4,12 @@ import Playgrounds from '@/components/sections/collab/desktop/Playgrounds';
 import Collaborate from '@/components/sections/collab/desktop/Collaborate';
 
 export default function CollabDesktop() {
-  
   return (
-    <div className='bg-(--mltBlack) w-full px-48 mx-auto flex flex-col gap-12 items-center justify-center overflow-hidden'>
-      <Hero/>
-      <AboutPlayground/>
-      <Playgrounds/>
-      <Collaborate/>
+    <div className='bg-(--mltBlack)'>
+      <Hero />
+      <AboutPlayground />
+      <Playgrounds />
+      <Collaborate />
     </div>
   );
 }
